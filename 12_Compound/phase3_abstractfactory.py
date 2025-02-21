b75ccb99bf984ba814b9a845efae0f938b5e6ae6
mizobata <bluesky@mizobata.com>
2025-02-21 23:39:49 +0900

    Compoundパターン開始。abstract factoryまで終了。

---
+++ 12_Compound/phase3_abstractfactory.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+import phase2_duck as duck
+import phase2_goose as goose
+import phase3_decorater as counter
+
+class AbstractDuckFactory(ABC):
+    @abstractmethod
+    def createMallardDuck(self):
+        pass
+    @abstractmethod
+    def createRedheadDuck(self):
+        pass
+    @abstractmethod
+    def createDuckCall(self):
+        pass
+    @abstractmethod
+    def createRubberDuck(self):
+        pass
+    @abstractmethod
+    def createGooseAdapter(self):
+        pass
+    
+class DuckFactory(AbstractDuckFactory):
+    def createMallardDuck(self):
+        return duck.MallardDuck()
+    
+    def createRedheadDuck(self):
+        return duck.RedheadDuck()
+    
+    def createDuckCall(self):
+        return duck.DuckCall()
+    
+    def createRubberDuck(self):
+        return duck.RubberDuck()
+    
+    def createGooseAdapter(self):
+        return duck.GooseAdapter(goose.Goose())
+    
+class CountingDuckFactory(AbstractDuckFactory):
+    def createMallardDuck(self):
+        return counter.QuackCounter(duck.MallardDuck())
+    
+    def createRedheadDuck(self):
+        return counter.QuackCounter(duck.RedheadDuck())
+    
+    def createDuckCall(self):
+        return counter.QuackCounter(duck.DuckCall())
+    
+    def createRubberDuck(self):
+        return counter.QuackCounter(duck.RubberDuck())
+    
+    def createGooseAdapter(self):
+        return counter.QuackCounter(duck.GooseAdapter(goose.Goose()))
+    
