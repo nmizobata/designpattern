7e43d6f21597b1f20e292d82a8a88f551db6af48
Norihito Mizobata <bluesky@mizobata.com>
2025-02-21 12:29:55 +0900

    stateパターン完了。

---
+++ 10_State/phase3_main.py
@@ -0,0 +1,33 @@
+import phase3_state as state
+
+GumballMachine = state.GumballMachine(5)
+
+print(GumballMachine)
+GumballMachine.insertQuarter()
+GumballMachine.turnCrank()
+
+print(GumballMachine)
+
+GumballMachine.insertQuarter()
+GumballMachine.ejectQuarter()
+GumballMachine.turnCrank()
+
+print(GumballMachine)
+
+GumballMachine.insertQuarter()
+GumballMachine.turnCrank()
+GumballMachine.insertQuarter()
+GumballMachine.turnCrank()
+GumballMachine.ejectQuarter()
+
+print(GumballMachine)
+
+GumballMachine.insertQuarter()
+GumballMachine.insertQuarter()
+GumballMachine.turnCrank()
+GumballMachine.insertQuarter()
+GumballMachine.turnCrank()
+GumballMachine.insertQuarter()
+GumballMachine.turnCrank()
+
+print(GumballMachine)
